ea0c4fd2f6178e980b93fe5217ee8953e2722b5b
Caleb Cramer <calebcramer@Calebs-MacBook-Pro.local>
2019-11-10 16:58:36 -0800

    figured out tabbed panes

--- src/AppView.java
+++ src/AppView.java
@@ -1,3 +1,15 @@
-public class AppView {
+import javax.swing.*;
+import java.awt.*;
 
+public class AppView extends JFrame {
+
+    public AppView(){
+        super("DIVDR");
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(400,400));
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new TabbedPanel(), BorderLayout.CENTER);
+        pack();
+    }
 }
---
+++ src/TabbedPanel.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TabbedPanel extends JPanel {
+    public TabbedPanel() {
+        super(new GridLayout(1, 1));
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+
+        //login panel
+        JComponent panel1 = new JPanel();
+        tabbedPane.addTab("Login", panel1);
+
+        //tutorial panel
+        JComponent panel2 = new JPanel();
+        tabbedPane.addTab("Tutorial", panel2);
+
+        //practice panel
+        JComponent panel3 = new JPanel();
+        tabbedPane.addTab("Practice Problems", panel3);
+
+        //quiz panel
+        JComponent panel4 = new JPanel();
+        tabbedPane.addTab("Quiz Yourself", panel4);
+
+        add(tabbedPane);
+
+        tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+    }
+}
